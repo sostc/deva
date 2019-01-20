fe198efe42fac0d5d1203c4300d0aaead62c1656
spark <zjw0358@gmail.com>
2019-01-20 18:46:08 +0800

    add setup ,move directory

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     author='spark',
     author_email='zjw0358@gmail.com',
-    url='http://github.com/sostc/deva',
+    url='https://github.com/sostc/deva',
     license='http://www.apache.org/licenses/LICENSE-2.0.html',
     description='data eval in future',
     install_requires=["requests", "streamz"]
