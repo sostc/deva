2829c8f48f148a9c7fa8d71df1c2d92377259846
spark <zjw0358@gmail.com>
2019-03-23 16:45:28 +0800

    发布0.2版本

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deva',
-    version=0.1,
+    version=0.2,
     packages=find_packages(),
     author='spark',
     author_email='zjw0358@gmail.com',
