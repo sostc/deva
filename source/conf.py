f0b4d6245f7ff8cdd4d7e484b6489d2afc474815
spark <zjw0358@gmail.com>
2020-03-08 00:41:35 +0800

    bug

--- source/conf.py
+++ source/conf.py
@@ -72,6 +72,9 @@ exclude_patterns = []
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+master_doc = 'index'
+
+github_doc_root = 'https://github.com/sostc/deva/tree/master/docs/'
 
 
 def setup(app):
