35223914798373f73b876cf998cda9a7130d03ad
zjw0358 <zjw0358@gmail.com>
2026-04-13 19:17:33 +0000

    feat: 生成项目Code Wiki文档
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/components/Empty.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/lib/utils'
-
-// Empty component
-export default function Empty() {
-  return (
-    <div className={cn('flex h-full items-center justify-center')}>Empty</div>
-  )
-}
